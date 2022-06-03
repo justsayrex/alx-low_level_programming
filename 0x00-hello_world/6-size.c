1bbeb7138196fd7c01f190d947b4798f4169b6e8
justsayrex <justsayrex@gmail.com>
2022-06-03 00:55:59 -0700

    Task ^ on C hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
-#!/bin/bash
 #include <stdio.h>
-#include <unistd.h>
-
 /**
- *main - prints, followed 
- *by a new line, to
- *standard error
+ *main - prints, followed
  *Return: Always 0 (Success)
- **/
+ */
 int main(void)
 {
-	   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-10\n", 59)
-		      return (1);
+char acharacter;
+int ainteger;
+long along;
+long long alonglong;
+float afloat;
+
+printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+return (0);
 }
