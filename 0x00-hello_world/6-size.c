dd94e255232305b81e05353685d38bac30ef7bf1
justsayrex <justsayrex@gmail.com>
2022-06-02 18:39:01 -0700

    0x00-C helloworld

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * *main - prints, followed 
+ * *by a new line, to
+ * *standard error
+ * *Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-10\n", 59)
+		      return (1);
+}
