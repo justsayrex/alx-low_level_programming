21e9f6c558ea6c33f4aba47a022285d44eab39f2
justsayrex <justsayrex@gmail.com>
2022-06-02 20:04:23 -0700

    task 5 C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,19 +5,10 @@
  *  *
  *   * Return: Alwways 0 (Success)
  **/
+
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-10\n", 59)	       	return (1);
 }
+
 
