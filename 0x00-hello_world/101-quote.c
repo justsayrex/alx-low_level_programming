a420bec533f6aa9e9791ed278678c0fefaf1f7f0
justsayrex <justsayrex@gmail.com>
2022-06-02 22:42:44 -0700

    Task 8 C qoutes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,19 @@
-
 #include <stdio.h>
 
 #include <unistd.h>
 
-/*
- *main - prints, followed
- *by a new line, to
- *standard error
- *Return: Always 0 (Success) 
+/**
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)
-{                                                                               	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-100\n",59)
-	return (1);
-}	   
 
+int main(void)
+
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
+	return (1);
+}
