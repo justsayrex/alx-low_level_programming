75af86735788d07f4ea3f4a3f0fa9be63f714234
justsayrex <justsayrex@gmail.com>
2022-06-03 18:13:21 -0700

    Task 8 on C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - main block
+ *Description:	prints all the numbers of base 16 in lowercase with putchar.
+ *Return: (0) that's cool.
+ */
+int main(void)
+{
+int i;
+char alph;
+for (i = 0 ; i < 10 ; i++)
+{putchar(i + '0'); }
+for (alph = 'a'; alph < 'g'; alph++)
+{putchar(alph); }
+putchar('\n');
+return (0);
+}
