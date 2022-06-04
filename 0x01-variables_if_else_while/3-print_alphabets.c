915d9593c0acb591ca057414bbf407d7b0f4067e
justsayrex <justsayrex@gmail.com>
2022-06-03 17:20:27 -0700

    Task 3 Alphabet C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description:print the character.
+ *Return:(0) that's great.
+ */
+int main(void)
+{
+char alph;
+for (alph = 'a' ; alph <= 'z' ; alph++)
+{putchar (alph); }
+for (alph = 'A' ; alph <= 'Z' ; alph++)
+{putchar (alph); }
+putchar ('\n');
+return (0);
+}
