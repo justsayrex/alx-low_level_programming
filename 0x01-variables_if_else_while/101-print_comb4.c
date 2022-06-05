c386235ee7bb798a2ef2d6a0a1a2fd05a4aa986c
justsayrex <justsayrex@gmail.com>
2022-06-05 03:40:54 -0700

    Task on C

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - main block
+ *Description: print all possible combinations of three different digits
+ *Return: (0) Thats so cool
+ */
+int main(void)
+{
+int x, y, z;
+for (x = 0; x <= 8; x++)
+{
+for (y = x + 1; y < 9; y++)
+{
+for (z = y + 1; z < 10; z++)
+{
+putchar((x % 10) + '0');
+putchar((y % 10) + '0');
+putchar((z % 10) + '0');
+if (x == 7 && y == 8 && z == 9)
+{continue; }
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
