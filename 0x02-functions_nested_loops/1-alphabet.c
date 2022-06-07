831c236c2a15e961715b4e7385d160a38ef4f93e
justsayrex <justsayrex@gmail.com>
2022-06-07 15:58:04 -0700

    Task on Nested Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet of letters
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int w;
+
+	for (w = 'a'; w <= 'z'; w++)
+	{
+		_putchar(w);
+	}
+	_putchar('\n');
+}
