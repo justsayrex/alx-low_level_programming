6fe1411568a6ea468198ee71fe312729a7df839e
justsayrex <justsayrex@gmail.com>
2022-06-07 15:22:35 -0700

    Task on Nested Loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints Putchar
+ * Description: A program that prints '_putchar', followed by a new line.
+ * Return = (0) success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my readme 0x02. C - Functions, nested loops
