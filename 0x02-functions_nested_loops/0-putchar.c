b4854beacb63a7c3a5bfb127709fba224eb054e8
justsayrex <justsayrex@gmail.com>
2022-06-07 15:37:02 -0700

    hotfix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - prints Putchar
- * Description: A program that prints '_putchar', followed by a new line.
- * Return = (0) success
+ * main - prints putchar
+ * Return: (0) success
  */
 int main(void)
 {
