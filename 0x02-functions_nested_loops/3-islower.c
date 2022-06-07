98886d8c1f6b8df216749991f8b34c4be4f5c30d
justsayrex <justsayrex@gmail.com>
2022-06-07 16:36:54 -0700

    Task on Nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
+ */
+int _islower(int g)
+{
+	return (g >= 'a' && g <= 'z');
+}
