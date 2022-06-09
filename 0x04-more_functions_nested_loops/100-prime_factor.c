93d5783798de6b8e5ea3662ecd6383fa1d9c2b0a
justsayrex <justsayrex@gmail.com>
2022-06-09 14:38:57 -0700

    Task on More function

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,39 +4,23 @@
 /**
  * main - finds and prints the largest prime factor of the number 612852475143
  * followed by a new line
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+	long int i, number = 612852475143;
 
-	n = 612852475143;
-
-	max = -1;
-
-	while (n % 2 == 0)
+	for (i  = 2; i <= number; i++)
 	{
-		max = 2;
-
-		n /= 2;
-	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
+		if (number % i == 0)
 		{
-			max = i;
-			n = n / i;
+			number = number / i;
+
+			i--;
 		}
 	}
-
-	if (n > 2)
-		max = n;
-	
-	printf("%ld\n", max);
+	printf("%lu\n", i);
 
 	return (0);
 }
