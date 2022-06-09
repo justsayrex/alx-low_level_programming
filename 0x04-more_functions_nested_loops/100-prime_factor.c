b6c0e533e4263865982e1032cdbd7d4008b04432
justsayrex <justsayrex@gmail.com>
2022-06-09 13:57:23 -0700

    Task 11 on More functions

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,6 +6,7 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	long int n;
@@ -13,13 +14,16 @@ int main(void)
 	long int i;
 
 	n = 612852475143;
+	
 	max = -1;
 
 	while (n % 2 == 0)
 	{
 		max = 2;
+		
 		n /= 2;
 	}
+	
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
@@ -27,6 +31,8 @@ int main(void)
 			max = i;
 			n = n / i;
 		}
+	}
+	
 	if (n > 2)
 		max = n;
 		
