71b401b0893b99bce07b9a432e5141504e3cfa98
justsayrex <justsayrex@gmail.com>
2022-06-13 16:22:00 -0700

    Task 9 on Pointer Arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,25 +2,22 @@
 #include <stdio.h>
 
 /**
- * _strcpy - update value.
- * @dest: value to be evaluate.
- * @src: value to be evaluate.
- * Return: not
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
+ * @dest: destination
+ * @src: source
+ * Return: the pointer to dest.
  */
-
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
-
-	while (*(src + l) != '\0')
+	int count = 0;
+	while (count >= 0)
 	{
-		l++;
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
 	}
-	for ( ; x < l ; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
 	return (dest);
 }
