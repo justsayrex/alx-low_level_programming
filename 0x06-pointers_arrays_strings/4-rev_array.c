05cbec0133f6f744a65cb962ddc30039303d1ba1
justsayrex <justsayrex@gmail.com>
2022-06-16 14:28:48 -0700

    Task 4 rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,13 +10,16 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
-	int temp;
+	int i, j, temp
 
-	for (i = 0, i < 10, i++;)
+	for (i = 0; i < n - 1; i++)
 	{
-		temp = a[i];
-		a[i] = a[n];
-		a[n] = temp;
+		for (j = i + 1; j > 0; j--)
+
+		{
+		temp = *(a + j);
+		*(a + j) = *(a + (j - 1));
+		*(a + (j - 1)) = temp;
+		}
 	}
 }
