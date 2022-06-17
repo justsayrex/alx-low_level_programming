c34674c069c177137f573f7f3fb3042b6b524ff7
justsayrex <justsayrex@gmail.com>
2022-06-16 18:02:28 -0700

    Hotfix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,16 +4,12 @@
  * reverse_array -> reversing an array
  * @a: array a
  * @n: an elements of array
- * #define n 13
  */
 
 void reverse_array(int *a, int n)
 {
 	int *p , i, aux, k;
 	
-	a = [1, 2, 3, 4];
-	aux = 1;
-	a = [4, 3, 2, 1];
 	p = a;
 	for (i = 0; i < n - 1; i++)
 		p++;
