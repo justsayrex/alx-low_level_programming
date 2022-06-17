f9601092cb28c9e7d375c9375b385499deb41e52
justsayrex <justsayrex@gmail.com>
2022-06-16 17:46:51 -0700

    Task on pointer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,6 +4,7 @@
  * reverse_array -> reversing an array
  * @a: array a
  * @n: an elements of array
+ * #define n 13
  */
 
 void reverse_array(int *a, int n)
@@ -11,14 +12,14 @@ void reverse_array(int *a, int n)
 	int *p , i, aux, k;
 	
 	a = [1, 2, 3, 4];
-	aux = 1
-	a = [4, 3, 2, 1]
+	aux = 1;
+	a = [4, 3, 2, 1];
 	p = a;
 	for (i = 0; i < n - 1; i++)
 		p++;
 	for (k = 0; k < n / 2; k++)
 	{
-		aux = a[k]
+		aux = a[k];
 		a[k] = *p;
 		*p = aux;
 		p--;
