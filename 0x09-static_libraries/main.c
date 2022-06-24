a5542f9ffb159689c7db69da6f40c1b1cf29c86d
justsayrex <justsayrex@gmail.com>
2022-06-24 16:43:06 -0700

    Task 0 :Static Libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/**
+ *  main - check out
+ *  return 0
+ */
+
 int main(void)
 {
-	 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	 return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n");
+	return (0);
 }
