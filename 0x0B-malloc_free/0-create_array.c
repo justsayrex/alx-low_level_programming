2b3b599d7438fe58b6eb4c6989d925e865706bab
justsayrex <justsayrex@gmail.com>
2022-06-28 06:59:22 -0700

    Update 0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return = (NULL);
+		return (NULL);
 
 	cr = malloc(sizeof(c) * size);
 
 	if (cr == NULL)
-		return = (NULL);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		cr[i] = c;
