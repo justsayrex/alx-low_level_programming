bf5f4e578d3133e3be400193d07f390d2a589573
justsayrex <justsayrex@gmail.com>
2022-06-28 07:11:46 -0700

    Task 1: strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	unsigned int i, j;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
@@ -22,7 +22,7 @@ char *_strdup(char *str)
 	strout = (char *)malloc(sizeof(char) * (i + 1));
 
 	if (strout == NULL)
-		return NULL;
+		return (NULL);
 
 	for (j = 0; j <= i; j++)
 		strout[j] = str[j];
