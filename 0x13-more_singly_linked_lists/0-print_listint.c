e82987a34be40a722b9acde158bda9545616f8b1
justsayrex <justsayrex@gmail.com>
2022-07-19 18:43:00 -0700

    Task 0 : Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
 	}
-	return (nodes);
+
+	return (nnodes);
 }
