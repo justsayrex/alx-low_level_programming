ff62d09b02d0ebb150c534db663905f5b91f37a2
justsayrex <justsayrex@gmail.com>
2022-07-25 15:37:23 -0700

    Task 1 : Under the snow

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,6 @@
  * standard output.
  * @filename: file.
  * @letters: Number of letters it should read and print.
- *
  * Return: Actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
